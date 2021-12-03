16993ca6cbb0702b1cb84a5fa1a30091f6e6b2c6
96arjan <66669796+96arjan@users.noreply.github.com>
2021-12-03 14:36:17 +0000

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -1,7 +1,6 @@
-NUM_LEN, PATH = 12, r"./data/day_3.txt"
-report = open(PATH, "r").read().split("\n")[:-1]
+report = open(r"./data/day_3.txt", "r").read().split("\n")[:-1]
 
-base = [0] * NUM_LEN
+base = [0] * 12
 for num in report: base = [int(i) + j for i, j in zip(num, base)]
 
 gamma = "".join(list(map(lambda x: "1" if x >= len(report) / 2 else "0", base)))
