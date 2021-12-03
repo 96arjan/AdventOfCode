fa571f19a0b23f66247873a38ed936b2eb2fb260
96arjan <66669796+96arjan@users.noreply.github.com>
2021-12-03 13:58:22 +0000

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -4,7 +4,7 @@ report = open(PATH, "r").read().split("\n")[:-1]
 base = [0] * NUM_LEN
 for num in report: base = [int(i) + j for i, j in zip(num, base)]
 
-gamma = list(map(lambda x: 1 if x >= len(report) / 2 else 0, base)).__str__()[1:-1].replace(", ", "")
-epsilon = list(map(lambda x: 1 if x < len(report) / 2 else 0, base)).__str__()[1:-1].replace(", ", "")
+gamma = "".join(list(map(lambda x: "1" if x >= len(report) / 2 else "0", base)))
+epsilon = "".join(list(map(lambda x: "1" if x < len(report) / 2 else "0", base)))
 
 print(int(gamma, 2) * int(epsilon, 2))
